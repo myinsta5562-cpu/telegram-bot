9b82abd1180f3acf6e13f91adae3ca7718d8f8e8
myinsta5562-cpu <myinsta5562@gmail.com>
2026-04-14 20:04:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ plans = {
     "plan1": {"name": "RP V!D€OS", "price": "99", "link": "https://t.me/+A_WqvGYW64kzMGM1"},
     "plan2": {"name": "€P V!D€OS (50K+)", "price": "149", "link": "https://t.me/+l5vBpSJUvuFiMTg1"},
     "plan3": {"name": "All in One (50 Groups)", "price": "249", "link": "https://t.me/+Pe4fTs485hc2MDRl"},
-    "plan4": {"name": "VIP PURE ALL (100K+)", "price": "499", "link": "https://t.me/+s4x5B2nHcZg0ZjM1"}
+    "plan4": {"name": "VIP PURE €P ALL (100K+)", "price": "499", "link": "https://t.me/+s4x5B2nHcZg0ZjM1"}
 }
 
 demo_videos = [
@@ -138,7 +138,7 @@ def callback(call):
             InlineKeyboardButton("👉 RP V!D€OS - ₹99", callback_data="buy_plan1"),
             InlineKeyboardButton("👉 €P V!D€OS - ₹149", callback_data="buy_plan2"),
             InlineKeyboardButton("👉 All in One (50 Groups) - ₹249", callback_data="buy_plan3"),
-            InlineKeyboardButton("👉 VIP PURE ALL Vdos (100K+ VIDEOS) - ₹499", callback_data="buy_plan4"),
+            InlineKeyboardButton("👉 VIP PURE €P ALL Vdos (100K+ VIDEOS) - ₹499", callback_data="buy_plan4"),
             InlineKeyboardButton("🔙 Back", callback_data="back_start")
         )
 
